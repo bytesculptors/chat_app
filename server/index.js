b718c35a69199692722038b3a0e582cf840f2a73
bytesculptors <tuannghia200603@gmail.com>
2024-07-04 16:58:05 +0700

    Add model for conversation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const authRoutes = require('./routes/auth.routes')
+const messageRoutes = require('./routes/message.routes')
 const connectToMongoDB = require('./db/connectToMongoDB')
 
 dotenv.config()
@@ -9,6 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 app.listen(port, () => {
     connectToMongoDB()
