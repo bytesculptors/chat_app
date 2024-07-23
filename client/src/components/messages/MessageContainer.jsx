a9a50e9c372968aeb4db95311487effde696b2b3
bytesculptors <tuannghia200603@gmail.com>
2024-07-23 16:57:27 +0700

    Add screen when users do not select any chat

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -4,7 +4,7 @@ import MessageInput from './MessageInput'
 import NoChatSelected from './NoChatSelected'
 
 const MessageContainer = () => {
-    const noChatSelected = true
+    const noChatSelected = false
     return (
         <div className='md:min-w-[450px] flex flex-col'>
             {noChatSelected ? <NoChatSelected /> : (
--- client/src/components/messages/NoChatSelected.jsx
+++ client/src/components/messages/NoChatSelected.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { TiMessage } from "react-icons/ti";
 
 const NoChatSelected = () => {
     return (
-        <div>NoChatSelected</div>
+        <div className='flex items-center justify-center w-full h-full'>
+            <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+                <p>Welcome 👋 you</p>
+                <p>Select a chat to start messaging</p>
+                <TiMessage className='text-3xl md:text-6xl text-center' />
+            </div>
+        </div>
     )
 }
 
